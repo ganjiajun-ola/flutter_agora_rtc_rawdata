4a64238b98d7d155fe716516123f577fc7f64463
Gan Jia Jun <gan.jiajun@olaparty.sg>
2023-08-03 16:18:40 +0800

    - include windows

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <agora_rtc_engine/agora_rtc_engine_plugin.h>
+#include <agora_rtc_rawdata/agora_rtc_rawdata_plugin.h>
 #include <iris_event/iris_event_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AgoraRtcEnginePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AgoraRtcEnginePlugin"));
+  AgoraRtcRawdataPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AgoraRtcRawdataPlugin"));
   IrisEventPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("IrisEventPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  agora_rtc_engine
+  agora_rtc_rawdata
   iris_event
 )
 
